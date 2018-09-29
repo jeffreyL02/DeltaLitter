affca18bbbed68ee0e33b297892497dfa283d0bf
Keith Kwong <chubbster7134@gmail.com>
2018-09-29 15:32:58 -0700

    vision api works, camera activated from nav page

--- public/Nav/nav.js
+++ public/Nav/nav.js
@@ -1,6 +1,53 @@
+//Enables invisible camera button
 let camera = document.getElementById('camera');
 let invisBtn = document.getElementById('invisBtn');
 
 camera.addEventListener('click', function(){
   invisBtn.click();
 });
+
+//Vision API
+let reader = new FileReader();
+let picture;
+//necessary json for API
+var img = {
+  "requests": [
+    {
+      "image": {
+        "content": ""
+      },
+      "features": [
+        {
+          "type": "LABEL_DETECTION"
+        }
+      ]
+    }
+  ]
+};
+let stringified;
+
+  reader.addEventListener('load', function(){
+    img.requests[0].image.content = btoa(reader.result);  //encodes the picture, puts in object img
+    console.log(img);
+    stringified = JSON.stringify(img);  //turns img into a string
+    httpPostAsync("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyChzwkDB5dABRGzGoUKQkSAzM5DCSVBhNw", stringified, function (text) { console.log(text); })
+  }, false);
+
+    invisBtn.addEventListener('change', function(){
+      invisBtn = document.getElementById("invisBtn");
+      picture = invisBtn.files[0];
+      console.log(picture);
+      reader.readAsBinaryString(picture);
+    })
+
+
+//HTTP requests function for API
+function httpPostAsync(theUrl, body, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("POST", theUrl, true); // true for asynchronous
+  xmlHttp.send(body);
+}
