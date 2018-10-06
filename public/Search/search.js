7055d9eb07bc5cfaf88d107be0a02aa656605130
William Trang <shadowskarmory17@gmail.com>
2018-10-05 17:34:50 -0700

    1/2 of search page

--- public/Search/search.js
+++ public/Search/search.js
@@ -1,19 +1,31 @@
 $( function(){
   var trash = [
-    "Plastic",
-    "Electronics",
-    "Toast",
-    "Breadboard",
-    "Leon",
-    "Trash",
-    "Chemistry",
-    "Mr Leon",
-    "Mystery Mix Lab",
+    "Plastic Bottle",
+    "Cell Phone",
+    "Battery",
+    "Chemicals",
+    "Clothing",
+    "Aluminum Can",
+    "Lego",
+    "Paper",
+    "Plastic Wrap",
     "The quick brown fox jumped over the lazy dog" //so you get something no matter what letter you type lmao
   ];
   $( "#inputTrash" ).autocomplete({ //jquery autocomplete function lmao??
     source: trash
  });
+});
+
+var inputTrash = document.getElementById("inputTrash");
+var searchBtn = document.getElementById("searchButton");
+var bottlePage = document.getElementById("postPage");
+var searchPage = document.getElementById("searchPage");
+
+searchButton.addEventListener("click",function(){
+  if(inputTrash.value ==  "Plastic Bottle"){
+    searchPage.style.display = 'none';
+    bottlePage.style.display = 'block';
+  }
 });
 
 let home = document.getElementById("homeBtn");
