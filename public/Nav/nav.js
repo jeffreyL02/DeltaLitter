c29b44156abadf4cd41de94043cb1b92266ee432
Keith Kwong <chubbster7134@gmail.com>
2018-10-10 16:19:18 -0700

    made event store name and under a random id

--- public/Nav/nav.js
+++ public/Nav/nav.js
@@ -6,42 +6,39 @@ keith put this in ur code when you pull the image into the html
 */
 
 //Enables invisible camera button
-const firebaseDB = firebase.database();
-let ref = firebaseDB.ref("pictures");
-
-window.scrollTo(0,1);
+window.scrollTo(0, 1);
 let camera = document.getElementById('camera');
 let invisBtn = document.getElementById('invisBtn');
 
-camera.addEventListener('click', function(){
+camera.addEventListener('click', function () {
   invisBtn.click();
 });
 
 //navigation portion
-let mapLink = document.getElementById("mapNav");
 let homeBtn = document.getElementById("homeBtn");
-homeBtn.addEventListener('click', function(){
+homeBtn.addEventListener('click', function () {
   document.getElementById("postPage").style.display = "none";
   document.getElementById("navPage").style.display = "block";
   homeBtn.style.display = "none";
-}) 
-mapLink.addEventListener('click', function(){
-  window.location.href = "../MapReal/map.html";
 })
 let search = document.getElementById("search");
 let map = document.getElementById("map");
-search.addEventListener('click', function(){
+search.addEventListener('click', function () {
   window.location.href = "../Search/search.html";
 })
-map.addEventListener('click', function(){
+map.addEventListener('click', function () {
   window.location.href = "../MapReal/map.html";
 });
+document.getElementById("mapNav").addEventListener('click', function(){
+  window.location.href = "../MapReal/map.html";
+})
+
 //Vision API
 let reader = new FileReader();
 let picture;
 
 //Info needed for image recognition
-let keyWords = ["glass", "electronic", "plastic", "aluminum"];
+let materialList = ["plastic", "glass", "electronic", "plastic", "aluminum"];
 let primaryTrash = ["bottle", "battery", "can"];
 let waterBottleGenInfo = "The plastic bottle is generally made of plastic and is used by people to drink water.";
 let batteryGenInfo = "Batteries work through electric currents to power certain items.";
@@ -52,8 +49,8 @@ let imgProfile = {
   name: "Unable to identify",
   genInfo: "There is currently no info on this item.",
   recyclability: "Unable to be recycled.",
-  dumpLocation:"",
-  reUseFactor:""
+  dumpLocation: "",
+  reUseFactor: ""
 }
 
 //necessary json for API
@@ -81,48 +78,42 @@ let currentLength;
 let id = "";
 
 //after the reader loads, passes encoded pict. into http request
-  reader.addEventListener('load', function(){
-    imgProfile = {
-      type: "Unknown Material",
-      name: "Unable to identify",
-      genInfo: "There is currently no info on this item.",
-      recyclability: "Unable to be recycled.",
-      dumpLocation: "",
-      reUseFactor: ""
-    }
-    console.log(id);
-    img.requests[0].image.content = btoa(reader.result);  //encodes the picture, puts in object img
-    firebaseDB.ref("pictures/" + id).set(img.requests[0].image.content).then(
-      function(){
-        console.log("data stored");
-      }).catch(function(error){
-        console.log(error);
-      });
-    console.log(img);
-    stringified = JSON.stringify(img);  //turns img into a string
-    currentLength = VisionDesc.length;
-    for (let i = 0; i <= currentLength; i++) {
-      VisionDesc.pop();
-      VisionScore.pop();
-    }
-    httpPostSync("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyChzwkDB5dABRGzGoUKQkSAzM5DCSVBhNw", stringified, function (text) {VisionInfo = JSON.parse(text); })
-    console.log(VisionInfo);
-    //pushes all elements of json info into custom arrays
-    for(let i = 0; i < VisionInfo.responses[0].labelAnnotations.length; i++){
-      VisionDesc.push(VisionInfo.responses[0].labelAnnotations[i].description);
-      VisionScore.push(VisionInfo.responses[0].labelAnnotations[i].score);
-    }
-    console.log(imgProfile);
-    createPostPage();
-  }, false);
+reader.addEventListener('load', function () {
+  imgProfile = {
+    type: "Unknown Material",
+    name: "Unable to identify",
+    genInfo: "There is currently no info on this item.",
+    recyclability: "Unable to be recycled.",
+    dumpLocation: "",
+    reUseFactor: ""
+  }
+  console.log(id);
+  img.requests[0].image.content = btoa(reader.result);  //encodes the picture, puts in object img
+  console.log(img);
+  stringified = JSON.stringify(img);  //turns img into a string
+  currentLength = VisionDesc.length;
+  for (let i = 0; i <= currentLength; i++) {
+    VisionDesc.pop();
+    VisionScore.pop();
+  }
+  httpPostSync("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyChzwkDB5dABRGzGoUKQkSAzM5DCSVBhNw", stringified, function (text) { VisionInfo = JSON.parse(text); })
+  console.log(VisionInfo);
+  //pushes all elements of json info into custom arrays
+  for (let i = 0; i < VisionInfo.responses[0].labelAnnotations.length; i++) {
+    VisionDesc.push(VisionInfo.responses[0].labelAnnotations[i].description);
+    VisionScore.push(VisionInfo.responses[0].labelAnnotations[i].score);
+  }
+  console.log(imgProfile);
+  createPostPage();
+}, false);
 
-    invisBtn.addEventListener('change', function(){
-      invisBtn = document.getElementById("invisBtn");
-      picture = invisBtn.files[0];  //reads image file
-      id = "";
-      id = createRandomID();
-      reader.readAsBinaryString(picture); //activates filerader to read image as binary string
-    })
+invisBtn.addEventListener('change', function () {
+  invisBtn = document.getElementById("invisBtn");
+  picture = invisBtn.files[0];  //reads image file
+  id = "";
+  id = createRandomID();
+  reader.readAsBinaryString(picture); //activates filerader to read image as binary string
+})
 
 
 //HTTP requests function for API
@@ -144,11 +135,13 @@ let reuseInfo = document.getElementById("reuseInfo");
 let title = document.getElementById("title");
 
 //creates the post page
-function createPostPage(){
+function createPostPage() {
   filterCommon();
   determineRecyclability();
   imgProfile.name = imgProfile.type;
-  imgProfile.genInfo = GenInfoList[determineGenfInfo()];
+  if(determineGenfInfo() >= 0){
+    imgProfile.genInfo = GenInfoList[determineGenfInfo()];
+  }
   postPic.src = window.URL.createObjectURL(picture);
   title.innerHTML = imgProfile.name;
   genInfo.innerHTML = imgProfile.genInfo;
@@ -161,12 +154,12 @@ function createPostPage(){
 //determines what general info to display
 let trashCount = 0;
 let handledDesc = "";
-function determineGenfInfo(){
-  for(let i = 0; i < VisionDesc.length; i++){
-    for(let j = 0; j < primaryTrash.length; j++){
-      while(trashCount < primaryTrash.length){
-        handledDesc =  VisionDesc[i].slice(VisionDesc[i].search(primaryTrash[trashCount]), VisionDesc[i].length);
-        if(handledDesc == primaryTrash[j]){
+function determineGenfInfo() {
+  for (let i = 0; i < VisionDesc.length; i++) {
+    for (let j = 0; j < primaryTrash.length; j++) {
+      while (trashCount < primaryTrash.length) {
+        handledDesc = VisionDesc[i].slice(VisionDesc[i].search(primaryTrash[trashCount]), VisionDesc[i].length);
+        if (handledDesc == primaryTrash[j]) {
           return j;
         }
         trashCount++;
@@ -179,17 +172,18 @@ function determineGenfInfo(){
 //determines recyclability and material of object
 let counter = 0;
 let reCounter = 0;
-function determineRecyclability(){
-  for (let i = 0; i < keyWords.length; i++) {
+function determineRecyclability() {
+  for (let i = 0; i < materialList.length; i++) {
     counter = 0;
     while (counter < VisionDesc.length) {
-        while(reCounter < keyWords.length){
-          if (VisionDesc[counter].slice(VisionDesc[counter].search(keyWords[reCounter]), keyWords[reCounter].length) == keyWords[i]) {
-            imgProfile.type = keyWords[i];
-            imgProfile.recyclability = "Able to be recycled.";
-          }
-          reCounter++;
+      while (reCounter < materialList.length) {
+        if (VisionDesc[counter].slice(VisionDesc[counter].search(materialList[reCounter]), materialList[reCounter].length) == materialList[i]) {
+          imgProfile.type = materialList[i];
+          imgProfile.recyclability = "Able to be recycled.";
+          return 0;
         }
+        reCounter++;
+      }
       counter++;
       reCounter = 0;
     }
@@ -199,23 +193,23 @@ function determineRecyclability(){
 //filter common vision results
 let commonCount = 0;
 let commonResults = ["product", "text"];
-function filterCommon(){
+function filterCommon() {
   commonCount = 0;
-  for(let i = 0; i < VisionDesc.length; i++){
+  for (let i = 0; i < VisionDesc.length; i++) {
     commonCount = 0;
-    while(commonCount < commonResults.length){
-        if(VisionDesc[i] == commonResults[commonCount]){
-          VisionDesc.splice(i, 1);
-          i--;
-        }
-        commonCount++;
+    while (commonCount < commonResults.length) {
+      if (VisionDesc[i] == commonResults[commonCount]) {
+        VisionDesc.splice(i, 1);
+        i--;
+      }
+      commonCount++;
     }
   }
 }
 
-function createRandomID(){
+function createRandomID() {
   let rand = "";
-  for(let i = 0; i < 10; i++){
+  for (let i = 0; i < 10; i++) {
     rand += Math.round((Math.random() * (20 + 1)) + 1).toString();
   }
   return rand;
